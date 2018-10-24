3c002cad03fa35f6bcf69098d14c2607e7471061
dougege <d1378480401@163.com>
2018-10-24 16:06:26 +0800

    第二次提交

--- src/com/dmm/git/FirstGit.java
+++ src/com/dmm/git/FirstGit.java
@@ -1,11 +1,26 @@
 package com.dmm.git;
 
-public class FirstGit {
+import com.dmm.pojo.User;
+
+public class FirstGit extends User {
+	
+	public void Sleep() {
+		System.out.println("葛优躺！！！");
+	}
 
 	public static void main(String[] args) {
 		
 		System.out.println("我的第一个上传到github的项目");
-
+		
+		User user =new User();
+		user.setId(1);
+		user.setUsername("流氓兔");
+		user.setPwd("123456");
+		System.out.println(user);
+		
+		FirstGit fGit=new FirstGit();
+		fGit.Sleep();
+		
 	}
 
 }
